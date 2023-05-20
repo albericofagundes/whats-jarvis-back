2291baba55fb940173d375a0d8a2dd148e46ff27
alberico fagundes <alberico@fagundes.live>
2023-05-20 13:33:47 -0300

    teste conexao

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 import { create } from "venom-bot";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+create({
+  session: "Whats-Jarvis",
+  multidevice: true,
+})
+  .then((client) => {
+    start(client);
+    console.log("Whats-Jarvis connected");
+  })
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+async function start(client) {
+  console.log("Start Client");
+}
